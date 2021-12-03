6f22de80674d8670236155da7dc93981d35f0133
adityachau30 <adityacha26@gmail.com>
2021-12-03 13:27:07 +0545

    day2

---
+++ day 2/2)find_bmi.py
@@ -0,0 +1,6 @@
+mass = int(input("enter your mass in kg"))
+height = int(input("enter your height in meter"))
+
+bmi = (mass/(height*height))
+
+print("the bmi of body is {} kg/m\u00b2".format(bmi))
---
+++ day 2/3)min_passed_since_midnight.py
@@ -0,0 +1,7 @@
+num = int(input("enter the time"))
+hour = int(num/60)
+min = (num%60)
+print(f"the hours is {hour}")
+print(f"the minute is{min}")
+
+
